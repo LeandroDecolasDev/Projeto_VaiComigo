2b8b05fd9962059cd8d3b407613a1930087c0b82
EzeGolder <eze.silva2003@gmail.com>
2025-10-29 12:14:17 -0300

    Commit inicial do app versão springboot

---
+++ src/main/java/com/vaic/vaic_springboot/VaicSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.vaic.vaic_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaicSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaicSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaic/vaic_springboot/VaicSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaic.vaic_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaicSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
