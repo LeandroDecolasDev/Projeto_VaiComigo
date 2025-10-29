763a0a6387365f2689c3a3d970175f5cd1fefbe8
EzeGolder <eze.silva2003@gmail.com>
2025-10-29 17:13:48 -0300

    Adição de funções post put basicas

--- src/main/java/com/vaic/vaic_springboot/controller/UsuarioController.java
+++ src/main/java/com/vaic/vaic_springboot/controller/UsuarioController.java
@@ -1,7 +1,37 @@
 package com.vaic.vaic_springboot.controller;
 
+import com.vaic.vaic_springboot.model.Usuario;
+import com.vaic.vaic_springboot.repository.UsuarioRepository;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/usarios")
 public class UsuarioController {
+private final UsuarioRepository repository;
+
+    public UsuarioController(UsuarioRepository repository){
+        this.repository = repository;
+    }
+
+    
+
+    @PostMapping
+    public Usuario cadastrar(@RequestBody Usuario novoUsuario){
+        return repository.save(novoUsuario);
+    }
+
+    @PutMapping("/{id}")
+    public Usuario atualizarDados(@PathVariable Long id, @RequestBody Usuario dadosNovos){
+        Usuario usuario = repository.findById(id)
+            .orElseThrow(()-> new RuntimeException("Usuário não encontrado"));
+        usuario.setNome(dadosNovos.getNome());
+        usuario.setEmail(dadosNovos.getEmail());
+        usuario.setCurso(dadosNovos.getCurso());
+        usuario.setTelefone(dadosNovos.getTelefone());
+
+        return repository.save(usuario);
+    }
+
 
 
 
